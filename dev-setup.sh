a03755d187a200ba0889ee8aa3d7630ecca379ae
Çağrı Altan <cagrialtanhd@gmail.com>
2023-11-26 18:11:34 +0300

    some updates

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-# openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out nginx.crt -keyout nginx.key
+# openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out ssl/nginx.crt -keyout ssl/nginx.key
 
 
 
