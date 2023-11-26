181c37bca83133920c3cea8f87846c9b87fa4d78
Çağrı Altan <cagrialtanhd@gmail.com>
2023-11-26 19:27:31 +0300

    dev script updates

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,6 +1,12 @@
-#!bin/bash
+#!/bin/bash
 
-# openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out ssl/nginx.crt -keyout ssl/nginx.key
+# Check if the keys are generated before. If yes skip the process, if no create them.
+if [ ! -f "./ssl/nginx.key" ] || [ ! -f "./ssl/nginx.crt" ]; then
+    openssl req -x509 -nodes -newkey rsa:2048 -keyout ./ssl/nginx.key -out ./ssl/nginx.crt -days 365 -subj "/CN=localhost" -batch
+fi
 
+# build docker-image
+sudo docker-compose build
 
-
+# starts services
+sudo docker-compose up
